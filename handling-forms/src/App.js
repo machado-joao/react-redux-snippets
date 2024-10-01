356b273a97d307ea7e004b03a7ff938f1ec6ce6f
machado-joao <joaolucas.cm@hotmail.com>
2024-10-01 08:59:45 -0300

    Handling forms

---
+++ handling-forms/src/App.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import BookList from "./components/BookList";
+import BookCreate from "./components/BookCreate";
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log(`Adding "${title}" book...`);
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 9999), title },
+    ];
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
+  const editBookById = (id, title) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title };
+      }
+      return book;
+    });
+    setBooks(updatedBooks);
+  };
+
+  return (
+    <div className="app">
+      <h1>Reading List</h1>
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
+}
+
+export default App;
---
+++ handling-forms/src/components/BookShow.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import BookEdit from "./BookEdit";
+
+function BookShow({ book, onDelete, onEdit }) {
+  const [showEdit, setShowEdit] = useState(false);
+
+  const handleDeleteClick = () => {
+    onDelete(book.id);
+  };
+
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+  const handleSubmit = (id, title) => {
+    setShowEdit(false);
+    onEdit(id, title);
+  };
+
+  let content = <h3>{book.title}</h3>;
+  if (showEdit) {
+    content = <BookEdit onSubmit={handleSubmit} book={book} />;
+  }
+
+  return (
+    <div className="book-show">
+      <img
+        src={`https://picsum.photos/seed/${book.id}/300/200`}
+        alt="Book cover."
+      />
+      <div>{content}</div>
+      <div className="actions">
+        <button className="edit" onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}></button>
+      </div>
+    </div>
+  );
+}
+
+export default BookShow;

4d29fc16fb33fdb25e9f84c77b8104fde0607f9d
machado-joao <joaolucas.cm@hotmail.com>
2024-10-01 16:43:39 -0300

    Using the context system

--- handling-forms/src/components/BookShow.js
+++ handling-forms/src/components/BookShow.js
@@ -1,20 +1,21 @@
 import { useState } from "react";
 import BookEdit from "./BookEdit";
+import useBooksContext from "../hooks/use-books-context";
 
-function BookShow({ book, onDelete, onEdit }) {
+function BookShow({ book }) {
   const [showEdit, setShowEdit] = useState(false);
+  const { deleteBookById } = useBooksContext();
 
   const handleDeleteClick = () => {
-    onDelete(book.id);
+    deleteBookById(book.id);
   };
 
   const handleEditClick = () => {
     setShowEdit(!showEdit);
   };
 
-  const handleSubmit = (id, title) => {
+  const handleSubmit = () => {
     setShowEdit(false);
-    onEdit(id, title);
   };
 
   let content = <h3>{book.title}</h3>;

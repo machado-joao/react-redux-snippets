ec3ad3ca351ec3e287ff9ba09c81b57eb0782e2d
machado-joao <joaolucas.cm@hotmail.com>
2025-08-17 13:39:17 -0300

    Managing slices

---
+++ managing-slices/src/index.js
@@ -0,0 +1,15 @@
+import "bulma/css/bulma.css";
+import "./styles.css";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import App from "./App";
+
+const element = document.getElementById("root");
+const root = createRoot(element);
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);

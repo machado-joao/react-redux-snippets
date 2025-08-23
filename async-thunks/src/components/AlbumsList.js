b37598b48cafbebf5e3fa3151da0e6a84806775b
machado-joao <joaolucas.cm@hotmail.com>
2025-08-23 13:12:44 -0300

    Implementing new features with RTK Query

--- async-thunks/src/components/AlbumsList.js
+++ async-thunks/src/components/AlbumsList.js
@@ -1,5 +1,38 @@
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import Skeleton from "./Skeleton";
+import AlbumsListItem from "./AlbumsListItem";
+import Button from "./Button";
+
 function AlbumsList({ user }) {
-  return <div>Albums for {user.name}</div>;
+  const { data, error, isFetching } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
+
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
+
+  let content;
+  if (isFetching) {
+    content = <Skeleton className="h-10 w-full" times={3} />;
+  } else if (error) {
+    content = <div>Error loading albums.</div>;
+  } else {
+    content = data.map((album) => {
+      return <AlbumsListItem key={album.id} album={album} />;
+    });
+  }
+
+  return (
+    <div>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">Albums for {user.name}</h3>
+        <Button loading={results.isLoading} onClick={handleAddAlbum}>
+          Add album
+        </Button>
+      </div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
